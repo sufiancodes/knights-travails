174293ecd9e1eb044d11b8630e20218ea9244d18
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-11-21 20:12:27 +0500

    Add logic for movment of knight
    the logic for movment of knight is add to genrate the positions that knight can move to from current one

--- main.rb
+++ main.rb
@@ -1,3 +1,19 @@
 def knight_move(start_point, end_point)
-  
+  start_position = start_point
+  adjacent = []
+  # up
+  adjacent << [start_position[1] + 2, start_position[0] - 1]
+  adjacent << [start_position[1] + 2, start_position[0] + 1]
+  # left
+  adjacent << [start_position[1] + 1, start_position[0] - 2]
+  adjacent << [start_position[1] - 1, start_position[0] - 2]
+  # right
+  adjacent << [start_position[1] + 1, start_position[0] + 2]
+  adjacent << [start_position[1] - 1, start_position[0] + 2]
+  # down
+  adjacent << [start_position[1] - 2, start_position[0] - 1]
+  adjacent << [start_position[1] - 2, start_position[0] + 1]
+  p adjacent
+  # [[5,2]] this is legal move
 end
+knight_move([3,3], [3,6])
