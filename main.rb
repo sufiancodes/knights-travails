17dbda02293c9be311bef09633005c31a3fd7065
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-11-22 19:50:23 +0500

    Knight_move method simplified by adding new method
    knight_move method simplified in two method new method genrate_move now do job of making moves

--- main.rb
+++ main.rb
@@ -1,19 +1,20 @@
-def knight_move(start_point, end_point)
-  start_position = start_point
+def generate_moves(origin)
   adjacent = []
   # up
-  adjacent << [start_position[1] + 2, start_position[0] - 1]
-  adjacent << [start_position[1] + 2, start_position[0] + 1]
+  adjacent << [origin[1] + 2, origin[0] - 1]
+  adjacent << [origin[1] + 2, origin[0] + 1]
   # left
-  adjacent << [start_position[1] + 1, start_position[0] - 2]
-  adjacent << [start_position[1] - 1, start_position[0] - 2]
+  adjacent << [origin[1] + 1, origin[0] - 2]
+  adjacent << [origin[1] - 1, origin[0] - 2]
   # right
-  adjacent << [start_position[1] + 1, start_position[0] + 2]
-  adjacent << [start_position[1] - 1, start_position[0] + 2]
+  adjacent << [origin[1] + 1, origin[0] + 2]
+  adjacent << [origin[1] - 1, origin[0] + 2]
   # down
-  adjacent << [start_position[1] - 2, start_position[0] - 1]
-  adjacent << [start_position[1] - 2, start_position[0] + 1]
+  adjacent << [origin[1] - 2, origin[0] - 1]
+  adjacent << [origin[1] - 2, origin[0] + 1]
   p adjacent
-  # [[5,2]] this is legal move
+end
+def knight_move(start_point, end_point)
+  generate_moves(start_point)
 end
 knight_move([3,3], [3,6])
