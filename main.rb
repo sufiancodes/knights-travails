8b2fc57715a9cb286af08bb12c54a3b49534b9dc
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-11-24 22:32:30 +0500

    Knight moves method negative problem solve
    Knight moves method was generating negative coordinates logic fix with new method tat remove the negative arrays form 2d array

--- main.rb
+++ main.rb
@@ -13,16 +13,15 @@ def generate_moves(origin)
   adjacent << [origin[1] - 2, origin[0] - 1]
   adjacent << [origin[1] - 2, origin[0] + 1]
 end
-def knight_move(start_point, end_point)
-  # p generate_moves(start_point)
-  
-end
-knight_move([0,0], [3,6])
-
-a = [[1,2],[1,-3],[7,8]]
 
 def remove_negative_from_2d_array(array)
     filteration = array.reject do |inner_array|
     inner_array.any? {|element| element < 0 }
   end
 end
+
+def knight_move(start_point, end_point)
+  remove_negative_from_2d_array(generate_moves(start_point))
+  
+end
+knight_move([3,3], [3,6])
