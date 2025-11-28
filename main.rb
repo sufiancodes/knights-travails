24372d5a446406107f3abc55a75bf4d9f84c88ed
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-11-28 20:31:33 +0500

    Add logic for tracking
    logic for tracking first_parent and its child is add

--- main.rb
+++ main.rb
@@ -23,7 +23,52 @@ def remove_negative_from_2d_array(array)
 end
 
 def knight_move(start_point, end_point)
-  p generate_moves(start_point)
-  
+  parent = {}
+  first_child = generate_moves(start_point)
+  first_child.each do |kid|
+    parent[kid] = start_point
+  end
+  first_child.each do |kid|
+    second_child = generate_moves(kid)
+    second_child.each do |kido|
+      parent[kido] = kid
+    end
+  end
+  pp parent
 end
-knight_move([0,0], [3,6])
+knight_move([0,0], [3,3])
+
+
+
+
+
+#  parent = {}
+#   target = [3,3]
+#   first_child_parent = [0,0]
+#   first_child_child = generate_moves(first_child_parent)
+#   first_child_child.each { |child| parent[child] = first_child_parent }
+#   puts "first_child Search"
+#   p parent
+
+#   first_child_child.each do |second_childparent|
+#   second_childchildren = generate_moves(second_childparent)
+#   puts "here are second parent"
+#   p second_childparent
+#   puts "here are second children"
+#   p second_childchildren
+  
+#   second_childchildren.each { |second_childchild| parent[second_childchild] = second_childparent }
+#   end
+  
+#   puts "Second search"
+#   pp parent
+
+  # path = []
+  # path << target
+  # node = parent[target]
+  # while node != first_child_parent
+  #   path << node
+  #   node = parent[node]
+  # end
+  # path << first_child_parent
+  # p path.reverse
